dcb4a33829ca2c48968cc7546de226d48fbef023
masudaaaaaaa <simon.huaume@protonmail.com>
2024-12-03 13:57:22 +0100

    print B_local

--- src/mpi/apps/tbsla_attention_mpi.cpp
+++ src/mpi/apps/tbsla_attention_mpi.cpp
@@ -25,6 +25,15 @@ double compute_gflops(double runtime, int nnz, int cols_B) {
     return (2.0 * nnz * cols_B) / (runtime * 1e9); // GFLOPS
 }
 
+void print_dense_matrix(double* M, int nb_row, int nb_col) {
+    for (int i=0; i<nb_row; i++){
+        for (int j=0; j<nb_col; j++){
+            std::cout << M[i*nb_col + j];
+        }
+            std::cout << std::endl;
+    }
+}
+
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
@@ -98,10 +107,14 @@ int main(int argc, char** argv) {
     // Local dense matrix block
     double* B_local = new double[ln_row * cols_B];
     distribute_dense_matrix(B, B_local, matrix_dim, cols_B, ln_row, MPI_COMM_WORLD);
+    std::cout << "$$$ B for rank: " << rank << std::endl;
+    print_dense_matrix(B_local, ln_row, cols_B);
+
 
     // Local result matrix
     double* C_local = new double[ln_row * cols_B];
     std::memset(C_local, 0, sizeof(double) * ln_row * cols_B);
+
 
     // Perform sparse-dense multiplication
     auto t_start = now();
